5daabadc56bf058465148762e382ffc22067608b
Thiago Sawada <tms1_tms2@hotmail.com>
2017-09-27 18:47:17 -0300

    Scrapping Presidencia da Republica database

--- scrapping.rb
+++ scrapping.rb
@@ -8,9 +8,8 @@ servidores_sem_salario = []
 filepath = 'data/servidores.json'
 filepath2 = 'data/servidores_sem_salario.json'
 
-
-(1..261).each do |page|
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=25201&Pagina=#{page}"
+(258..272).each do |page|
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=20101&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
@@ -25,8 +24,16 @@ filepath2 = 'data/servidores_sem_salario.json'
 
     cargo = html.search("#listagemConvenios tr td:nth-child(2)")[28]
 
-    unless cargo
-      cargo = html.search("#listagemConvenios tr td:nth-child(2)")[1]
+    if cargo && !cargo.text.include?("*")
+      cargo = cargo.text.strip
+    else
+      cargo = html.search("#listagemConvenios tr td:nth-child(2)")[1].text.strip
+
+      if cargo == ""
+        descricao = html.search("#listagemConvenios tr td:nth-child(2)")[2]
+        atividade = html.search("#listagemConvenios tr td:nth-child(2)")[3]
+        cargo = "#{descricao.text.strip} - #{atividade.text.strip}"
+      end
     end
 
     link_da_remuneracao = html.search("#resumo a")[0]
@@ -42,8 +49,9 @@ filepath2 = 'data/servidores_sem_salario.json'
 
     print "="
 
+
     if salario_l
-      servidores << { name: name, job: cargo.text.strip, salary_b: salario_b.text, salary_l: salario_l.text  }
+      servidores << { name: name, job: cargo, salary_b: salario_b.text, salary_l: salario_l.text  }
     else
       servidores_sem_salario << name
     end
