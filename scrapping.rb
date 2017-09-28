fb10702751315497e1007da5a9155dfc6f90e307
Thiago Sawada <tms1_tms2@hotmail.com>
2017-09-28 11:22:08 -0300

    Scrapping MEC database

--- scrapping.rb
+++ scrapping.rb
@@ -8,7 +8,7 @@ servidores_sem_salario = []
 filepath = 'data/servidores.json'
 filepath2 = 'data/servidores_sem_salario.json'
 
-(893..2030).each do |page|
+(953..2030).each do |page|
 
   print page
 
