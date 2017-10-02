1b284ef2e89e2af7838ac7d48ae3ac49639bd9d2
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-02 13:38:59 -0300

    Scrapping MRE database

--- scrapping.rb
+++ scrapping.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores.json'
 filepath2 = 'data/servidores_sem_salario.json'
 
-(1..146).each do |page|
+(199..220).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=59000&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=35000&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
--- scrapping2.rb
+++ scrapping2.rb
@@ -8,7 +8,7 @@ servidores_sem_salario = []
 filepath = 'data/servidores2.json'
 filepath2 = 'data/servidores_sem_salario2.json'
 
-(2358..15338).each do |page|
+(2636..15338).each do |page|
 
   print page
 
