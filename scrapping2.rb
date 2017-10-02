b3302f5928d8d889327200a181f0acab1f6ce584
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-01 21:59:59 -0300

    Scrapping MDIC database

--- scrapping2.rb
+++ scrapping2.rb
@@ -8,7 +8,7 @@ servidores_sem_salario = []
 filepath = 'data/servidores2.json'
 filepath2 = 'data/servidores_sem_salario2.json'
 
-(565..15338).each do |page|
+(870..15338).each do |page|
 
   print page
 
