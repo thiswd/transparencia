25e809ce3dbbdc540d50562d9821e6afe77e150f
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-03 10:47:45 -0300

    Scrapping embrapa database

--- scrapping2.rb
+++ scrapping2.rb
@@ -8,7 +8,7 @@ servidores_sem_salario = []
 filepath = 'data/servidores2.json'
 filepath2 = 'data/servidores_sem_salario2.json'
 
-(3208..15338).each do |page|
+(4373..15338).each do |page|
 
   print page
 
