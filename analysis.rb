5fba2942c568376343cd3de145798f205fc44847
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-07 19:17:24 -0300

    Scrapping serpro database

--- analysis.rb
+++ analysis.rb
@@ -13,6 +13,8 @@ total_employees = 0
 
 orgs = %w(agu anac anp anvisa bc cgu cvm ebc embrapa fiocruz funai ibge inss iphan mapa mcid mctic mdic mds me mec mf mi minc mj mma mme mp mre ms mt mte mtur pf pr)
 
+high_wages = []
+
 orgs.each do |org|
   filepath = "data/servidores_#{org}.json"
 
@@ -25,6 +27,7 @@ orgs.each do |org|
   sum_l = 0
   employee_count = 0
   porcentage_positive = []
+  high_wages = []
 
   employees.each do |employee|
 
@@ -44,39 +47,68 @@ orgs.each do |org|
     end
 
     if porcentage > 70 && porcentage < 100000
-
       porcentage_positive << { name: employee[:name], porcentage: porcentage }
     end
+
+    if salary_l > 39000
+      high_wages << employee
+    end
+
   end
 
   filepath = "data/servidores_sem_salario_#{org}.json"
 
   serialized_employees = File.read(filepath)
 
-  no_wage_employees = JSON.parse(serialized_employees)
+  no_wage_employees = JSON.parse(serialized_employees, symbolize_names: true)
 
   puts "========================================================="
-  puts "Médias - #{org.upcase}"
-  puts
-  puts "Salário bruto - R$ #{(sum_b/employee_count).round(2)}"
-  puts "Salário líquido - R$ #{(sum_l/employee_count).round(2)}"
-  puts "Empregados com salário - #{employee_count}"
-  puts "Empregados sem salário - #{no_wage_employees.count}"
-  puts "Total - #{employees.count}"
-  puts "---------------------------------------------------------"
-  puts "Variação acima de 70% no #{org.upcase}"
-  puts
+  puts "#{org.upcase}"
+  puts "========================================================="
 
-  porcentage_positive.each do |employee|
-    if employee.key?(:link)
-      puts "#{employee[:name]} - #{employee[:porcentage]}% - #{employee[:link]}"
+
+  no_wage_employees.each do |employee|
+
+    if employee.class == Hash
+      print "#{employee[:name]}, "
     else
-      puts "#{employee[:name]} - #{employee[:porcentage]}%"
+      print "#{employee}, "
     end
+
   end
-  puts "========================================================="
-  puts
+  # puts "========================================================="
+  # puts "Empregados sem salário - #{no_wage_employees.count}"
+  # puts
+
+
+  # puts "========================================================="
+  # puts "Médias - #{org.upcase}"
+  # puts
+  # puts "Salário bruto - R$ #{(sum_b/employee_count).round(2)}"
+  # puts "Salário líquido - R$ #{(sum_l/employee_count).round(2)}"
+  # puts "Empregados com salário - #{employee_count}"
+  # puts "Total - #{employees.count}"
+  # puts "---------------------------------------------------------"
+  # puts "Variação acima de 70% no #{org.upcase}"
+  # puts
+
+  # porcentage_positive.each do |employee|
+  #   if employee.key?(:link)
+  #     puts "#{employee[:name]} - #{employee[:porcentage]}% - #{employee[:link]}"
+  #   else
+  #     puts "#{employee[:name]} - #{employee[:porcentage]}%"
+  #   end
+  # end
+  # puts "========================================================="
+  # puts
+
+  # high_wages.each do |employee|
+  #   puts "#{employee[:name]} - #{employee[:job]} - #{employee[:salary_l]}"
+  # end
+
 
 end
 
-puts "Total de servidores: #{total_employees}"
+  # puts high_wages.size
+  # puts
+# puts "Total de servidores: #{total_employees}"
--- scrapping.rb
+++ scrapping.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores.json'
 filepath2 = 'data/servidores_sem_salario.json'
 
-(1..71).each do |page|
+(542..604).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=24204&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=25207&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
@@ -23,7 +23,6 @@ filepath2 = 'data/servidores_sem_salario.json'
 
     raw_html = open(url_s).read
     html = Nokogiri::HTML(raw_html)
-
 
     cargo = html.search("#listagemConvenios tr td:nth-child(2)")[28]
 
