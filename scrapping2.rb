7a53529877616ee6fc0c06c62249bea352d6c237
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-07 21:23:52 -0300

    Scrapping ancine database

--- scrapping2.rb
+++ scrapping2.rb
@@ -8,7 +8,7 @@ servidores_sem_salario = []
 filepath = 'data/servidores2.json'
 filepath2 = 'data/servidores_sem_salario2.json'
 
-(9965..15338).each do |page|
+(10035..15338).each do |page|
 
   print page
 
