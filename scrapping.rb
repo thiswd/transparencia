95821194e52980a03b8a6e0c238fd5895b2bf902
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-14 00:28:31 -0300

    Scrapping ibama database

--- scrapping.rb
+++ scrapping.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores.json'
 filepath2 = 'data/servidores_sem_salario.json'
 
-(3597..4906).each do |page|
+(1..207).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=21000&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=36205&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
