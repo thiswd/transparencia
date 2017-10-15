1eb2fff197e39acf149cf237fc92838f5876864b
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-15 14:28:47 -0200

    Scrapping inep database

--- scrapping2.rb
+++ scrapping2.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores2.json'
 filepath2 = 'data/servidores_sem_salario2.json'
 
-(2734..5559).each do |page|
+(1..74).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=70000&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=32100&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
