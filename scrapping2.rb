2218fdfd726fb49e6c0b7083a90dbf4a19cce336
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-15 19:41:21 -0200

    Scrapping cefetrj database

--- scrapping2.rb
+++ scrapping2.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores2.json'
 filepath2 = 'data/servidores_sem_salario2.json'
 
-(1..105).each do |page|
+(1..68).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26256&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26230&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
