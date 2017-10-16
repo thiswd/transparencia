7d90a8a2815d20cb4fa3372e2b280a8b5d02852e
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-16 20:19:53 -0200

    Scrapping ifma database

--- scrapping.rb
+++ scrapping.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores.json'
 filepath2 = 'data/servidores_sem_salario.json'
 
-(1..135).each do |page|
+(1..68).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26432&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26430&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
