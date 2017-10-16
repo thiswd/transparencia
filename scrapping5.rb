da72ca35cc8943be8c10949175208430750cc2c9
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-16 20:14:46 -0200

    Scrapping ifes database

--- scrapping5.rb
+++ scrapping5.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores5.json'
 filepath2 = 'data/servidores_sem_salario5.json'
 
-(1..225).each do |page|
+(1..180).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26405&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26435&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
