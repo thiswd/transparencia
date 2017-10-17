b8da6a898c7de5c161227755447eab16162edeff
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-17 13:55:20 -0200

    Scrapping uftm database

--- scrapping.rb
+++ scrapping.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores.json'
 filepath2 = 'data/servidores_sem_salario.json'
 
-(1..443).each do |page|
+(1..239).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26240&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26231&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
