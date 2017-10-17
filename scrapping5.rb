9bf209515a23cf75abe2f64bbbe9928cbf20eb38
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-16 22:41:32 -0200

    Scrapping ifgo database

--- scrapping5.rb
+++ scrapping5.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores5.json'
 filepath2 = 'data/servidores_sem_salario5.json'
 
-(1..44).each do |page|
+(1..183).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26442&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26249&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
