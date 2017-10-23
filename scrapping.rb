9cce37a65d7b3edec854cdccee4f5fcfb4357a6c
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-23 21:59:37 -0200

    Scrapping funarte database

--- scrapping.rb
+++ scrapping.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores.json'
 filepath2 = 'data/servidores_sem_salario.json'
 
-(423..999).each do |page|
+(1..25).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26245&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=44205&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
