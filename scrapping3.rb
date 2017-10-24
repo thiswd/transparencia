878ec1ce9b0d5859282a66721506b59d55ba6b3c
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-24 12:45:45 -0200

    Scrapping prf database

--- scrapping3.rb
+++ scrapping3.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores3.json'
 filepath2 = 'data/servidores_sem_salario3.json'
 
-(1..535).each do |page|
+(1..275).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=26236&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=20101&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
