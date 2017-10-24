a8feea9e4231d5c03164f37acd2df4d6dc73bb3b
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-24 15:50:19 -0200

    Scrapping mec database again

--- scrapping5.rb
+++ scrapping5.rb
@@ -8,11 +8,11 @@ servidores_sem_salario = []
 filepath = 'data/servidores5.json'
 filepath2 = 'data/servidores_sem_salario5.json'
 
-(1..260).each do |page|
+(1..938).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=25201&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=20115&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
