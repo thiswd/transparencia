91bfc079d156bad148495bbf893feb4ab3fe0cbc
Thiago Sawada <tms1_tms2@hotmail.com>
2017-10-25 17:34:44 -0200

    Scrapping epe database

--- scrapping4.rb
+++ scrapping4.rb
@@ -7,12 +7,12 @@ servidores = []
 servidores_sem_salario = []
 filepath = 'data/servidores4.json'
 filepath2 = 'data/servidores_sem_salario4.json'
-# aglo
-(1..5).each do |page|
+# enap
+(1..16).each do |page|
 
   print page
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=51000&Pagina=#{page}"
+  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=40202&Pagina=#{page}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
