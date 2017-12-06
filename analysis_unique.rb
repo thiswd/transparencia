6c3cfde580d07c8800d3c43ea07e3d3ad2bd991d
Thiago Sawada <tms1_tms2@hotmail.com>
2017-12-06 17:28:54 -0200

    Delete repeated database

--- analysis_unique.rb
+++ analysis_unique.rb
@@ -15,7 +15,7 @@ def strFormat(n)
   n.gsub(/[.,]/, '.' => "", "," => ".")
 end
 
-orgs = %w(aglo agu2 ana anac anatel ancine aneel anp antaq anvisa bc2 capes cbtu cefetmg cefetrj ceitec cgu cnen cnpq codevasf conab cp2 cprm cvm dnit dnocs dnpm dpu eb ebc ebserh embrapa embratur enap epe fab fiocruz fnde fosorio funag funai funarte funasa fundacentro fundaj furg hcpa ibama ibc ibge2 ibram ifac ifal ifam ifap ifb ifba ifbaiano ifc ifce ifes iff iffarroupilha ifg ifgo ifma ifmg ifms ifmt ifnmg ifpa ifpb ifpe ifpi ifpr ifrj ifrn ifro ifrr ifrs ifsc ifse ifsertao ifsp ifsudestemg ifsul ifsuldeminas iftm ifto inep ines inmetro inpi inss ipea iphan mapa2 mb mcid mctic mdic mds me mec2 mf2 mi minc2 mj2 mma mme2 mp mpdft mpf mpt mre ms2 mt mte mtur nuclep pf2 pr2 previc prf serpro sudam sudeco sudene suframa susep tst ufabc ufac ufal ufam ufba ufc ufcg ufcspa ufersa ufes uff uffs ufg ufgd ufjf ufla ufma ufmg ufms ufmt ufob ufop ufopa ufpa ufpb ufpe ufpel ufpi ufpr ufra ufrb ufrgs ufrj ufrj2 ufrn ufrpe ufrr ufrrj ufs ufsb ufsc ufscar ufsj ufsm uft uftm ufu ufv ufvjm unb unifal unifap unifei unifesp unila unilab unipampa unir univasf utfpr valec)
+orgs = %w(aglo agu ana anac anatel ancine aneel anp antaq anvisa bc capes cbtu cefetmg cefetrj ceitec cgu cnen cnpq codevasf conab cp2 cprm cvm dnit dnocs dnpm dpu eb ebc ebserh embrapa embratur enap epe fab fiocruz fnde fosorio funag funai funarte funasa fundacentro fundaj furg hcpa ibama ibc ibge ibram ifac ifal ifam ifap ifb ifba ifbaiano ifc ifce ifes iff iffarroupilha ifg ifgo ifma ifmg ifms ifmt ifnmg ifpa ifpb ifpe ifpi ifpr ifrj ifrn ifro ifrr ifrs ifsc ifse ifsertao ifsp ifsudestemg ifsul ifsuldeminas iftm ifto inep ines inmetro inpi inss ipea iphan mapa mb mcid mctic mdic mds me mec mf mi minc mj mma mme mp mpdft mpf mpt mre ms mt mte mtur nuclep pf pr previc prf serpro sudam sudeco sudene suframa susep tst ufabc ufac ufal ufam ufba ufc ufcg ufcspa ufersa ufes uff uffs ufg ufgd ufjf ufla ufma ufmg ufms ufmt ufob ufop ufopa ufpa ufpb ufpe ufpel ufpi ufpr ufra ufrb ufrgs ufrj ufrj2 ufrn ufrpe ufrr ufrrj ufs ufsb ufsc ufscar ufsj ufsm uft uftm ufu ufv ufvjm unb unifal unifap unifei unifesp unila unilab unipampa unir univasf utfpr valec)
 
 orgs.each do |org|
 
@@ -76,4 +76,4 @@ puts "Sem salário: #{e_group_no_wages.size}"
 puts "Sem salário únicos: #{e_group_no_wages_uniq.size}"
 puts
 puts "Total: #{total}"
-puts "Proporção sem salário: #{rate}"
+puts "Proporção sem salário: #{rate}%"
