478c7eb11c1aafac73135137a47300e73e5a81df
Thiago Sawada <tms1_tms2@hotmail.com>
2023-07-17 22:42:35 -0300

    Scrapper Class

--- salarios/scrapping.rb
+++ salarios/scrapping.rb
@@ -1,82 +1,105 @@
-require "open-uri"
-require "nokogiri"
-require "json"
+require 'open-uri'
+require 'nokogiri'
+require 'json'
 
-pages_amount = ARGV[0]
+class Scrapper
+  attr_reader :servants, :wages_not_found, :pages_amount
 
-servers = []
-wages_not_found = []
-servers_path = "data/servers.json"
-without_wages_path = "data/wages_not_found.json"
-base_url = "https://www.portaltransparencia.gov.br"
-servers_route = "/servidores/"
+  BASE_URL = 'https://www.portaltransparencia.gov.br'
+  SERVANTS_ROUTE = '/servidores/'
+  INFO_ID = '#listagemConvenios'
 
-(1..pages_amount).each do |page_number|
+  def initialize(pages_amount)
+    @pages_amount = pages_amount
+    @servants = []
+    @wages_not_found = []
+  end
 
-  print page_number
+  def perform
+    (1..pages_amount).each do |page_number|
+      print page_number
 
-  url = "#{base_url}#{servers_route}OrgaoExercicio-ListaServidores.asp?CodOrg=17000&Pagina=#{page_number}"
-  raw_html = open(url).read
-  html = Nokogiri::HTML(raw_html)
+      page_url = construct_url(page_number)
+      html = fetch_html(page_url)
 
-  html.search("#listagem table td:nth-child(2) a").each do |server|
-    name = server.text.strip
-    path = server.attr("href")
-    server_url = base_url + servers_route + path
-
-    raw_html = open(server_url).read
-    html = Nokogiri::HTML(raw_html)
-
-    info_id = "#listagemConvenios"
-
-    position = html.search("#{info_id} tr td:nth-child(2)")[28]
-
-    if position && !position.text.include?("*")
-      position = position.text.strip
-    else
-      position = html.search("#{info_id} tr td:nth-child(2)")[1].text.strip
-
-      if position == ""
-        description = html.search("#{info_id} tr td:nth-child(2)")[2]
-        activity = html.search("#{info_id} tr td:nth-child(2)")[3]
-        position = "#{description.text.strip} - #{activity.text.strip}"
+      html.search("#listagem table td:nth-child(2) a").each do |servant|
+        process_servant(servant)
       end
+
+      write_to_file('data/servants.json', servants)
+      write_to_file('data/wages_not_found.json', wages_not_found)
     end
+  end
 
-    go_to_remuneration = html.search("#resumo a")[0]
-    path = go_to_remuneration.attr("href")
+  private
 
-    url = base_url + path
+  def construct_url(page_number)
+    "#{BASE_URL}#{SERVANTS_ROUTE}OrgaoExercicio-ListaServidores.asp?CodOrg=17000&Pagina=#{page_number}"
+  end
+
+  def fetch_html(url)
     raw_html = open(url).read
-    html = Nokogiri::HTML(raw_html)
+    Nokogiri::HTML(raw_html)
+  end
 
-    gross_salary_element = html.search("#{info_id} tbody tr:nth-child(5) .colunaValor")
+  def process_servant(servant)
+    name = servant.text.strip
+    path = servant.attr("href")
+    servant_url = BASE_URL + SERVANTS_ROUTE + path
 
+    html = fetch_html(servant_url)
+    position = set_position(html)
+
+    path = html.search("#resumo a")[0].attr("href")
+
+    remuneration_url = BASE_URL + path
+    html = fetch_html(remuneration_url)
+
+    gross_salary_element = html.search("#{INFO_ID} tbody tr:nth-child(5) .colunaValor")
     net_salary_element = html.search(".remuneracaolinhatotalliquida .colunaValor")[0]
 
     print "."
 
     if net_salary_element
-      server = {
+      servant = {
         name: name,
         job: position,
         gross_salary: gross_salary_element.text,
         net_salary: net_salary_element.text,
-        link: server_url
+        link: servant_url
       }
 
-      servers << server
+      servants << servant
     else
-      wages_not_found << { name: name, job: position, link: server_url }
+      wages_not_found << { name: name, job: position, link: servant_url }
     end
   end
 
-  File.open(servers_path, "wb") do |file|
-    file.write(JSON.generate(servers))
+  def write_to_file(path, content)
+    File.open(path, "wb") do |file|
+      file.write(JSON.generate(content))
+    end
   end
 
-  File.open(without_wages_path, "wb") do |file|
-    file.write(JSON.generate(wages_not_found))
-  end
+  def set_position(html)
+    position = html.search("#{INFO_ID} tr td:nth-child(2)")[28]
 
+    if position && !position.text.include?("*")
+      position = position.text.strip
+    else
+      position = html.search("#{INFO_ID} tr td:nth-child(2)")[1].text.strip
+
+      if position == ""
+        description = html.search("#{INFO_ID} tr td:nth-child(2)")[2]
+        activity = html.search("#{INFO_ID} tr td:nth-child(2)")[3]
+        position = "#{description.text.strip} - #{activity.text.strip}"
+      end
+    end
+
+    position
+  end
 end
+
+pages_amount = ARGV[0]
+scrapper = Scrapper.new(pages_amount)
+scrapper.perform
