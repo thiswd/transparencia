f1cec69ddd7b65c3d11454bad15c60b7b35c2dce
Thiago Sawada <tms1_tms2@hotmail.com>
2023-07-17 22:13:10 -0300

    refactor

--- salarios/scrapping.rb
+++ salarios/scrapping.rb
@@ -8,33 +8,37 @@ servers = []
 wages_not_found = []
 servers_path = "data/servers.json"
 without_wages_path = "data/wages_not_found.json"
+base_url = "https://www.portaltransparencia.gov.br"
+servers_route = "/servidores/"
 
 (1..pages_amount).each do |page_number|
 
   print page_number
 
-  url = "http://www.portaltransparencia.gov.br/servidores/OrgaoExercicio-ListaServidores.asp?CodOrg=17000&Pagina=#{page_number}"
+  url = "#{base_url}#{servers_route}OrgaoExercicio-ListaServidores.asp?CodOrg=17000&Pagina=#{page_number}"
   raw_html = open(url).read
   html = Nokogiri::HTML(raw_html)
 
   html.search("#listagem table td:nth-child(2) a").each do |server|
     name = server.text.strip
     path = server.attr("href")
-    server_url = "http://www.portaltransparencia.gov.br/servidores/#{path}"
+    server_url = base_url + servers_route + path
 
     raw_html = open(server_url).read
     html = Nokogiri::HTML(raw_html)
 
-    position = html.search("#listagemConvenios tr td:nth-child(2)")[28]
+    info_id = "#listagemConvenios"
+
+    position = html.search("#{info_id} tr td:nth-child(2)")[28]
 
     if position && !position.text.include?("*")
       position = position.text.strip
     else
-      position = html.search("#listagemConvenios tr td:nth-child(2)")[1].text.strip
+      position = html.search("#{info_id} tr td:nth-child(2)")[1].text.strip
 
       if position == ""
-        description = html.search("#listagemConvenios tr td:nth-child(2)")[2]
-        activity = html.search("#listagemConvenios tr td:nth-child(2)")[3]
+        description = html.search("#{info_id} tr td:nth-child(2)")[2]
+        activity = html.search("#{info_id} tr td:nth-child(2)")[3]
         position = "#{description.text.strip} - #{activity.text.strip}"
       end
     end
@@ -42,11 +46,11 @@ without_wages_path = "data/wages_not_found.json"
     go_to_remuneration = html.search("#resumo a")[0]
     path = go_to_remuneration.attr("href")
 
-    url = "http://www.portaltransparencia.gov.br#{path}"
+    url = base_url + path
     raw_html = open(url).read
     html = Nokogiri::HTML(raw_html)
 
-    gross_salary_element = html.search("#listagemConvenios tbody tr:nth-child(5) .colunaValor")
+    gross_salary_element = html.search("#{info_id} tbody tr:nth-child(5) .colunaValor")
 
     net_salary_element = html.search(".remuneracaolinhatotalliquida .colunaValor")[0]
 
